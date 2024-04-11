a4bc14dfc765569e108916331f6e634ca8edd5d9
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-04-11 19:18:27 +0300

    Defining an interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Map from "./components/Map";
-import LocationSearh from "./components/LocationSearch";
+import LocationSearch from "./components/LocationSearch";
 
 function App() {
   return (
-    <div className="m-5">
-      <LocationSearh />
-      <Map />
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className="col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
     </div>
   );
 }
