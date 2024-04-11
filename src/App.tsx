837feff4d204609b1fcee4bd0f36e5761c9cb864
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-04-11 19:08:48 +0300

    Component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Map from "./components/Map";
+import LocationSearh from "./components/LocationSearch";
+
 function App() {
-  return <div className="m-5">Hello there!</div>;
+  return (
+    <div className="m-5">
+      <LocationSearh />
+      <Map />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/LocationSearch.tsx
@@ -0,0 +1,3 @@
+export default function LocationSearh() {
+  return <div>Location</div>;
+}
