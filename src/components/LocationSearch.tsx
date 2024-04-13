2a389e4aa23c386a188617476e027c06e6bd5332
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-04-13 17:27:37 +0300

    Form Submissions with TypeScript

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -7,6 +7,26 @@ interface LocationSearchProps {
 
 export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const [places, setPlaces] = useState<Place[]>([]);
+  const [term, setTerm] = useState("");
 
-  return <div>Location</div>;
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("search api", term);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className="font-bold" htmlFor="term">
+          Search
+        </label>
+        <input
+          className="border border-gray-300 rounded-md shadow-md focus:border-indigo-500 px-4 py-2 w-full"
+          value={term}
+          id="term"
+          onChange={(e) => setTerm(e.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
