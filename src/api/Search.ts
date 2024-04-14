2f35b0670de7f725fb69a00d4b6fa5e3d1c9a3ed
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-04-14 15:59:20 +0300

    rendering locations

--- src/api/Search.ts
+++ src/api/Search.ts
@@ -16,7 +16,7 @@ export const search = async (term: string) => {
     `https://nominatim.openstreetmap.org/search?q=${term}&format=geojson&addressdetails=1&layer=address&limit=5`
   );
   const data: SearchResponse = await res.json();
-  const places: Place[] = data.features.map(() => {
+  const places: Place[] = data.features.map((feature) => {
     return {
       id: feature.properties.place_id,
       name: feature.properties.display_name,
