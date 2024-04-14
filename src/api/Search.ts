1de148e7659b6c640cfdd6d004e3168dc2914210
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-04-14 12:25:22 +0300

    create Search interface

--- src/api/Search.ts
+++ src/api/Search.ts
@@ -1,3 +1,15 @@
+interface SearchResponse {
+  features: {
+    geometry: {
+      coordinates: number[];
+    };
+    properties: {
+      place_id: number;
+      display_name: string;
+    };
+  };
+}
+
 export const search = async (term: string) => {
   const res = await fetch(
     `https://nominatim.openstreetmap.org/search?q=${term}&format=geojson&addressdetails=1&layer=address&limit=5`
