38a2d38b7b94752e645a2cf2487c510312caa59b
OĞULCAN DOĞAN <ogulcan.dgn@hotmail.com>
2024-04-14 16:07:28 +0300

    create react-leaflet

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,8 @@
+import "leaflet/dist/leaflet.css";
 import type { Place } from "../api/Place";
+import type { Map as leaflet } from "leaflet";
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 
 interface MapProps {
   place: Place | null;
